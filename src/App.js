9ccf54601f4c6241f1f1698a1c463dcb5176b85a
priyanshu6386 <priyanshuv624@gmail.com>
2023-01-12 19:04:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import TypingBox from "./Components/TypingBox";
+import {GlobalStyles} from "./Styles/global";
+var randomWords = require('random-words');
+
+function App() {
+
+  const words = randomWords(50);
+ 
+  return (
+    <div className="canvas">
+      
+      <GlobalStyles/>
+      <h1>Header</h1>
+      <TypingBox words ={words}/>
+      <h1>Footer</h1>
+    </div>
+  );
+}
+
+export default App;
